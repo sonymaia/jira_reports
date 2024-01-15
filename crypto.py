82e1612a83200c61d8c80a0477b04366cd1f5f7b
Sony Maia <sonymaia@MacBook-Pro-de-Sony.local>
2024-01-15 17:15:00 -0300

    crypto update

--- crypto.py
+++ crypto.py
@@ -5,6 +5,9 @@ def encrypt_or_decrypt(data, encrypt=True):
     try:
         key = os.getenv('KEY').encode('utf-8')
         cipher_suite = Fernet(key)
+
+        if isinstance(data, memoryview):
+            data = data.tobytes()
         
         if not isinstance(data, bytes):
             data = data.encode('utf-8')
@@ -16,7 +19,7 @@ def encrypt_or_decrypt(data, encrypt=True):
         
         return text
 
-    except InvalidToken as e:
+    except Exception as e:
         # Lidar com o erro aqui, por exemplo, imprimir uma mensagem de log
         print(f"Erro durante a operação de {'criptografia' if encrypt else 'descriptografia'}: {e}")
         return ""
